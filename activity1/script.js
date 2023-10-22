79a15fe2c942ae7b0667613bd5aae3bb3b940179
Kristoffe Angelo P. Ambe <118610639+kianAmbe@users.noreply.github.com>
2023-10-22 14:41:42 +0800

    patch

--- activity1/script.js
+++ activity1/script.js
@@ -4,28 +4,28 @@ document.addEventListener('DOMContentLoaded', function() {
     registrationForm.addEventListener('submit', function(e) {
         e.preventDefault();
 
-        // Get user input
+      
         const username = document.getElementById('username').value;
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
 
-        // Create user object
+     
         const user = {
             username: username,
             email: email,
             password: password
         };
 
-        // Store user data in local storage
+       
         localStorage.setItem('user', JSON.stringify(user));
 
-        // Clear the form
+       
         registrationForm.reset();
 
-        // You can provide a confirmation message or redirect the user to another page
+        
         alert('Registration successful!');
 
-        // For demonstration purposes, you can log the user data to the console
+     
         console.log(JSON.parse(localStorage.getItem('user')));
     });
 });
