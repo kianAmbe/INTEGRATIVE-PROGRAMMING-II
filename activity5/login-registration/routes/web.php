c54f57339d572628bc8e7e50b4ed690a14b88134
Yorfger <2100782@lnu.edu.ph>
2023-10-30 19:10:41 +0800

    addedact5

---
+++ activity5/login-registration/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('/dashboard', function () {
+    return view('blank_dashboard'); 
+});
+
+
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+
+require __DIR__.'/auth.php';
